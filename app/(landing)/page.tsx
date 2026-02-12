7dc72ebea85a2dcc627f9ad3f961654899e177b3
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-02-12 14:26:29 +0530

    feat 01 : Initial Setup

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+    </div>
+  );
+}
