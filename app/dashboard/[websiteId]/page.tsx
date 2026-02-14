29e569d1fa520414b5f5837eb625b596143dbaa7
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-02-15 00:36:57 +0530

    feat 06 : Implemented a top bar with date range presets and Installation guide

--- app/dashboard/[websiteId]/page.tsx
+++ app/dashboard/[websiteId]/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+
+import MainTopBar from "@/components/common/main-top-bar";
+import { startOfMonth } from "date-fns";
+import { useState } from "react";
+import { DateRange } from "react-day-picker";
 
 const page = () => {
-  return <div>WebsiteId Page</div>;
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: startOfMonth(new Date()),
+    to: new Date(),
+  });
+  return (
+    <div className="w-full flex flex-col space-y-1">
+      {/* Main Top */}
+      <MainTopBar dateRange={dateRange} setDateRange={setDateRange} />
+      <div className="w-full space-y-4  pb-4">
+        {/* Analytics Chart */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Locations */}
+          <div>Location</div>
+          {/*  Devices*/}
+          <div>Devices</div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {/* Top Sources  */}
+        <div>Top Sources</div>
+        {/*  Top Pages*/}
+        <div>Top Pages</div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
